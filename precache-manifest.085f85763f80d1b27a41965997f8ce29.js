bf9ce22c4127c7dd1a1245586f6c35eab39d3f35
sarkarsubhasish207 <sarkarsubhasish207@gmail.com>
2020-03-10 17:37:20 +0530

    Updates

---
+++ precache-manifest.085f85763f80d1b27a41965997f8ce29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea90b993568e0f55d87b7d76ff79f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a56bfe3a32a18c4fe55d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "331566fe78fe54ecdcc6",
+    "url": "/static/js/2.1aeccfc9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1aeccfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56bfe3a32a18c4fe55d",
+    "url": "/static/js/main.55aacc02.chunk.js"
+  },
+  {
+    "revision": "4048671a254f3ff29e1c",
+    "url": "/static/js/runtime-main.f904db64.js"
+  }
+]);
